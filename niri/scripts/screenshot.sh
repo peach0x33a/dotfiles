a5e49410b97219cece7784d3858549d30c8deaf3
peach0x33a <safeluren@outlook.com>
2025-12-02 19:25:48 +0800

    add: niri 25.11 完美的配置文件和截图 add: fcitx5 为什么也在？ modify: zshrc remove jetbra onekey env

--- niri/scripts/screenshot.sh
+++ niri/scripts/screenshot.sh
@@ -5,7 +5,7 @@ ACTION="$1"
 
 # 检查参数是否有效，如果无效则设置为默认值
 if [ "$ACTION" != "screenshot" ] && [ "$ACTION" != "screenshot-window" ] && [ "$ACTION" != "screenshot-screen" ]; then
-  ACTION="screenshot"
+    ACTION="screenshot"
 fi
 
 # 获取当前剪贴板的 SHA-256 校验和
@@ -16,8 +16,23 @@ niri msg action "$ACTION"
 
 # 等待剪贴板内容发生变化
 while [ "$(wl-paste | shasum)" = "$CLIPNOW" ]; do
-  sleep .05
+    sleep .05
 done
 
-# 将新的剪贴板内容（截屏图片）传递给 satty
-wl-paste | satty -f -
+#!/bin/bash
+
+EDIT_ACTION_KEY="editScreenshot"
+# 发送通知，并捕获用户点击的动作 KEY
+# notify-send 会将用户点击的动作 KEY 输出到标准输出
+clicked_action=$(notify-send "截图成功" \
+    "图片已复制到剪贴板" \
+    --urgency=low \
+    --expire-time=10000 \
+--action="$EDIT_ACTION_KEY=编辑截图")
+
+if [ "$clicked_action" = "$EDIT_ACTION_KEY" ]; then
+    # echo "will send to satty" > /tmp/satty-log.log
+    # 如果点击了，则将剪贴板内容（截屏图片）传递给 satty
+    wl-paste | satty --action-on-enter save-to-clipboard --actions-on-right-click exit --copy-command wl-copy --early-exit -f -
+fi
+
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -97,9 +97,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-___MY_VMOPTIONS_SHELL_FILE="${HOME}/.jetbrains.vmoptions.sh"; if [ -f "${___MY_VMOPTIONS_SHELL_FILE}" ]; then . "${___MY_VMOPTIONS_SHELL_FILE}"; fi
-
-
-#export GOOGLE_CLOUD_PROJECT="project2-470516"
-
 alias ssh="kitten ssh"
